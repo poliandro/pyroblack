792cb056b699337766fc8f5c6d948fa9e3d0327c
Gustavo Santana <43628256+gustavosta@users.noreply.github.com>
2024-02-04 17:54:51 +0100

    Fix `AttributeError` in `ChatType` filters
    (cherry picked from commit c83050513fb3f4f757ee9430e04002c910644adc)

--- pyrogram/filters.py
+++ pyrogram/filters.py
@@ -448,6 +448,7 @@ media_spoiler = create(media_spoiler_filter)
 
 # region private_filter
 async def private_filter(_, __, m: Message):
+    m = m.message if isinstance(m, CallbackQuery) else m
     return bool(m.chat and m.chat.type in {enums.ChatType.PRIVATE, enums.ChatType.BOT})
 
 
@@ -459,9 +460,8 @@ private = create(private_filter)
 
 # region group_filter
 async def group_filter(_, __, m: Message):
-    return bool(
-        m.chat and m.chat.type in {enums.ChatType.GROUP, enums.ChatType.SUPERGROUP}
-    )
+    m = m.message if isinstance(m, CallbackQuery) else m
+    return bool(m.chat and m.chat.type in {enums.ChatType.GROUP, enums.ChatType.SUPERGROUP})
 
 
 group = create(group_filter)
@@ -472,6 +472,7 @@ group = create(group_filter)
 
 # region channel_filter
 async def channel_filter(_, __, m: Message):
+    m = m.message if isinstance(m, CallbackQuery) else m
     return bool(m.chat and m.chat.type == enums.ChatType.CHANNEL)
 
 
