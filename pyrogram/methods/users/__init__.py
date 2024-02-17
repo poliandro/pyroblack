c43950f8210754d9ef85d52747841322931ac32d
KurimuzonAkuma <arturgayauskas@gmail.com>
2024-02-17 08:14:12 +0100

    Add new method check_username

--- pyrogram/methods/users/__init__.py
+++ pyrogram/methods/users/__init__.py
@@ -17,20 +17,21 @@
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 from .block_user import BlockUser
+from .check_username import CheckUsername
 from .delete_profile_photos import DeleteProfilePhotos
 from .delete_stories import DeleteStories
 from .edit_story import EditStory
 from .export_story_link import ExportStoryLink
 from .forward_story import ForwardStory
+from .get_all_stories import GetAllStories
 from .get_chat_photos import GetChatPhotos
 from .get_chat_photos_count import GetChatPhotosCount
 from .get_common_chats import GetCommonChats
 from .get_default_emoji_statuses import GetDefaultEmojiStatuses
 from .get_me import GetMe
-from .get_all_stories import GetAllStories
+from .get_peer_stories import GetPeerStories
 from .get_stories import GetStories
 from .get_stories_history import GetUserStoriesHistory
-from .get_peer_stories import GetPeerStories
 from .get_users import GetUsers
 from .send_story import SendStory
 from .set_emoji_status import SetEmojiStatus
@@ -42,6 +43,7 @@ from .update_profile import UpdateProfile
 
 class Users(
     BlockUser,
+    CheckUsername,
     DeleteStories,
     EditStory,
     ExportStoryLink,
