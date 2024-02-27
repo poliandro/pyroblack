4b364edea6fe611ad20ae71ebf5c9abb4634f7d3
eyMarv <eyMarv07@gmail.com>
2024-02-27 22:00:23 +0100

    fix getattr
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/types/messages_and_media/message.py
+++ pyrogram/types/messages_and_media/message.py
@@ -1258,9 +1258,7 @@ class Message(Object, Update):
                 else:
                     parsed_message.reply_to_story_id = message.reply_to.story_id
                     parsed_message.reply_to_story_user_id = (
-                        message.reply_to.peer.getattr(
-                            "user_id", message.reply_to.peer.getattr("channel_id", None)
-                        )
+                        getattr(message.reply_to.peer, "user_id", getattr(message.reply_to.peer, "channel_id", None))
                     )
 
                 if replies:
