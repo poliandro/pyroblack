7d1a25cd48eb3a1e2736d97e29e1ebb594414a26
KurimuzonAkuma <arturgayauskas@gmail.com>
2024-03-06 13:21:17 +0100

    Update check_username method
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/methods/users/check_username.py
+++ pyrogram/methods/users/check_username.py
@@ -16,17 +16,26 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import Union
+
 import pyrogram
 from pyrogram import raw
 
 
 class CheckUsername:
-    async def check_username(self: "pyrogram.Client", username: str) -> bool:
+    async def check_username(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        username: str
+    ) -> bool:
         """Check if a username is available.
 
         .. include:: /_includes/usable-by/users.rst
 
         Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+
             username (``str``):
                 Username to check.
 
@@ -36,9 +45,22 @@ class CheckUsername:
         Example:
             .. code-block:: python
 
-                await app.check_username("username")
+                await app.check_username("me", "username")
         """
+        peer = await self.resolve_peer(chat_id)
 
-        return bool(
-            await self.invoke(raw.functions.account.CheckUsername(username=username))
-        )
+        if isinstance(peer, raw.types.InputPeerChannel):
+            r = await self.invoke(
+                raw.functions.channels.CheckUsername(
+                    channel=peer,
+                    username=username
+                )
+            )
+        else:
+            r = await self.invoke(
+                raw.functions.account.CheckUsername(
+                    username=username
+                )
+            )
+
+        return bool(r)
