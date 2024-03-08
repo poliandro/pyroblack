6968a0929a5bc61c540a5428ee0e9a88829978b6
shriMADhav U k <hetzner@shrimadhavuk.me>
2024-03-08 13:43:29 +0100

    Expose _raw in RPCError
    Follow-Up: 66e29f9
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/errors/rpc_error.py
+++ pyrogram/errors/rpc_error.py
@@ -38,6 +38,7 @@ class RPCError(Exception):
         rpc_name: str = None,
         is_unknown: bool = False,
         is_signed: bool = False,
+        _raw = None
     ):
         super().__init__(
             "Telegram says: [{}{} {}] - {} {}".format(
@@ -48,6 +49,8 @@ class RPCError(Exception):
                 f'(caused by "{rpc_name}")' if rpc_name else "",
             )
         )
+
+        self._raw = _raw
 
         try:
             self.value = int(value)
@@ -74,26 +77,32 @@ class RPCError(Exception):
                 rpc_name=rpc_name,
                 is_unknown=True,
                 is_signed=is_signed,
+                _raw=rpc_error
             )
 
         error_id = re.sub(r"_\d+", "_X", error_message)
 
         if error_id not in exceptions[error_code]:
             raise getattr(
-                import_module("pyrogram.errors"), exceptions[error_code]["_"]
-            )(
-                value=f"[{error_code} {error_message}]",
-                rpc_name=rpc_name,
-                is_unknown=True,
-                is_signed=is_signed,
-            )
+                import_module("pyrogram.errors"),
+                exceptions[error_code]["_"]
+            )(value=f"[{error_code} {error_message}]",
+              rpc_name=rpc_name,
+              is_unknown=True,
+              is_signed=is_signed,
+              _raw=rpc_error)
 
         value = re.search(r"_(\d+)", error_message)
         value = value.group(1) if value is not None else value
 
         raise getattr(
-            import_module("pyrogram.errors"), exceptions[error_code][error_id]
-        )(value=value, rpc_name=rpc_name, is_unknown=False, is_signed=is_signed)
+            import_module("pyrogram.errors"),
+            exceptions[error_code][error_id]
+        )(value=value,
+          rpc_name=rpc_name,
+          is_unknown=False,
+          is_signed=is_signed,
+          _raw=rpc_error)
 
 
 class UnknownError(RPCError):
