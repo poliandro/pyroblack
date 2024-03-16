a6ea26414496e864d075c738f4515a28d8e108dc
KurimuzonAkuma <arturgayauskas@gmail.com>
2024-03-16 11:27:06 +0300

    Add new get_stickers method

--- pyrogram/methods/messages/__init__.py
+++ pyrogram/methods/messages/__init__.py
@@ -37,6 +37,7 @@ from .get_discussion_replies import GetDiscussionReplies
 from .get_discussion_replies_count import GetDiscussionRepliesCount
 from .get_media_group import GetMediaGroup
 from .get_messages import GetMessages
+from .get_stickers import GetStickers
 from .read_chat_history import ReadChatHistory
 from .retract_vote import RetractVote
 from .search_global import SearchGlobal
@@ -76,6 +77,7 @@ class Messages(
     ForwardMessages,
     GetMediaGroup,
     GetMessages,
+    GetStickers,
     SendAudio,
     SendChatAction,
     SendContact,
