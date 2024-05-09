533cad328494717f163c6523e8baf924164a81d8
KurimuzonAkuma <arturgayauskas@gmail.com>
2024-05-09 22:19:20 +0200

    Fix requested_chats
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/types/bots_and_keyboards/requested_chats.py
+++ pyrogram/types/bots_and_keyboards/requested_chats.py
@@ -16,7 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import List
+from typing import List, Union
 
 import pyrogram
 from pyrogram import enums
@@ -50,13 +50,17 @@ class RequestedChats(Object):
 
     @staticmethod
     def _parse(
-        client, action: "raw.types.MessageActionRequestedPeer"
+            client,
+            action: Union[
+                "raw.types.MessageActionRequestedPeer",
+                "raw.types.MessageActionRequestedPeerSentMe"
+            ]
     ) -> "RequestedChats":
         _requested_chats = []
 
         for requested_peer in action.peers:
-            chat_id = utils.get_peer_id(requested_peer)
-            peer_type = utils.get_peer_type(chat_id)
+            peer_id = utils.get_peer_id(requested_peer)
+            peer_type = utils.get_peer_type(peer_id)
 
             if peer_type == "user":
                 chat_type = enums.ChatType.PRIVATE
@@ -66,7 +70,15 @@ class RequestedChats(Object):
                 chat_type = enums.ChatType.CHANNEL
 
             _requested_chats.append(
-                types.Chat(id=chat_id, type=chat_type, client=client)
+                types.Chat(
+                    id=peer_id,
+                    type=chat_type,
+                    first_name=getattr(requested_peer, "first_name", None),
+                    last_name=getattr(requested_peer, "last_name", None),
+                    username=getattr(requested_peer, "username", None),
+                    photo=types.ChatPhoto._parse(client, getattr(requested_peer, "photo", None), peer_id, 0),
+                    client=client
+                )
             )
 
         return RequestedChats(
