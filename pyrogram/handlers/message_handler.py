b2d8eaef0e5463b822a3265c132ad596f42ce45e
wulan17 <wulan17@nusantararom.org>
2024-06-02 11:38:31 +0200

    Revert "pyrofork: handlers: message: check: fix errors when message.chat or message.from_user is None"
    This reverts commit c8768f65cfd8af88fde65ad5d930ab64e9c6957e.
    
    Signed-off-by: wulan17 <wulan17@nusantararom.org>
    
    (cherry picked from commit ad4dc01c8d00340d033b866ceb0897091cef6002)
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/handlers/message_handler.py
+++ pyrogram/handlers/message_handler.py
@@ -110,10 +110,8 @@ class MessageHandler(Handler):
         :return: Whether the message has a matching listener or handler and its filters does match with the Message.
         """
         listener_does_match = (
-            (await self.check_if_has_matching_listener(client, message))[0]
-            if message.chat is not None and message.from_user is not None
-            else False
-        )
+            await self.check_if_has_matching_listener(client, message)
+        )[0]
 
         if callable(self.filters):
             if iscoroutinefunction(self.filters.__call__):
@@ -140,11 +138,9 @@ class MessageHandler(Handler):
         :param args: Arguments to call the callback with.
         :return: None
         """
-        listener_does_match, listener = (
-            await self.check_if_has_matching_listener(client, message)
-            if message.chat is not None and message.from_user is not None
-            else False
-        ), None
+        listener_does_match, listener = await self.check_if_has_matching_listener(
+            client, message
+        )
 
         if listener and listener_does_match:
             client.remove_listener(listener)
