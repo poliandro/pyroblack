67d6786c9303cd0bb2410034b55ae5df0f18b2b9
KurimuzonAkuma <arturgayauskas@gmail.com>
2024-06-04 12:36:59 +0200

    Add support for deleting scheduled messages
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/methods/messages/delete_messages.py
+++ pyrogram/methods/messages/delete_messages.py
@@ -28,6 +28,7 @@ class DeleteMessages:
         chat_id: Union[int, str],
         message_ids: Union[int, Iterable[int]],
         revoke: bool = True,
+        is_scheduled: bool = None
     ) -> int:
         """Delete messages, including service messages.
 
@@ -49,6 +50,11 @@ class DeleteMessages:
                 channels and supergroups are always revoked (i.e.: deleted for everyone).
                 Defaults to True.
 
+            is_scheduled (``bool``, *optional*):
+                If True, the message will be deleted from the scheduled messages.
+                For userbots only.
+                Defaults to None.
+
         Returns:
             ``int``: Amount of affected messages
 
@@ -63,13 +69,23 @@ class DeleteMessages:
 
                 # Delete messages only on your side (without revoking)
                 await app.delete_messages(chat_id, message_id, revoke=False)
+
+                # Delete scheduled messages
+                await app.delete_messages(chat_id, message_id, is_scheduled=True)
         """
         peer = await self.resolve_peer(chat_id)
         message_ids = (
             list(message_ids) if not isinstance(message_ids, int) else [message_ids]
         )
 
-        if isinstance(peer, raw.types.InputPeerChannel):
+        if is_scheduled:
+            r = await self.invoke(
+                raw.functions.messages.DeleteScheduledMessages(
+                    peer=peer,
+                    id=message_ids
+                )
+            )
+        elif isinstance(peer, raw.types.InputPeerChannel):
             r = await self.invoke(
                 raw.functions.channels.DeleteMessages(channel=peer, id=message_ids)
             )
@@ -78,4 +94,4 @@ class DeleteMessages:
                 raw.functions.messages.DeleteMessages(id=message_ids, revoke=revoke)
             )
 
-        return r.pts_count
+        return len(r.updates[0].messages) if is_scheduled else r.pts_count
