1c994ac6fae10e6fcb0a7015deb14304ad6d0b2a
eyMarv <eyMarv07@gmail.com>
2024-06-16 21:45:47 +0200

    partly revert
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/session/session.py
+++ pyrogram/session/session.py
@@ -475,6 +475,10 @@ class Session:
         if self.client.instant_stop:
             return  # stop instantly
 
+        try:
+            await asyncio.wait_for(self.is_started.wait(), self.WAIT_TIMEOUT)
+        except asyncio.TimeoutError:
+            pass
 
         if isinstance(
             query, (raw.functions.InvokeWithoutUpdates, raw.functions.InvokeWithTakeout)
@@ -488,11 +492,6 @@ class Session:
         while retries > 0:
             if self.client.instant_stop:
                 return  # stop instantly
-
-            try:
-                await asyncio.wait_for(self.is_started.wait(), self.WAIT_TIMEOUT)
-            except asyncio.TimeoutError:
-                pass
 
             try:
                 return await self.send(query, timeout=timeout)
