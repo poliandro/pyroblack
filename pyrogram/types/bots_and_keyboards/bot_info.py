5c43c153026a2eee089e16951072bf4a3f9734a5
wulan17 <wulan17@nusantararom.org>
2024-08-18 15:23:38 +0200

    pyrofork: Add privacy_policy_url field to class BotInfo
    Signed-off-by: wulan17 <wulan17@nusantararom.org>
    
    (cherry picked from commit 945290692e7f98a11cd1b166b1c54cc1cbf2cfa3)
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/types/bots_and_keyboards/bot_info.py
+++ pyrogram/types/bots_and_keyboards/bot_info.py
@@ -34,19 +34,24 @@ class BotInfo(Object):
 
         description (``str``):
             Description of the bot;
+
+        privacy_policy_url (``str``, *optional*):
+            Privacy policy URL of the bot.
     """
 
-    def __init__(self, name: str, about: str, description: str):
+    def __init__(self, name: str, about: str, description: str, privacy_policy_url: str = None):
         super().__init__()
 
         self.name = name
         self.about = about
         self.description = description
+        self.privacy_policy_url = privacy_policy_url
 
     @staticmethod
     def _parse(bot_info: "raw.types.bots.BotInfo") -> "BotInfo":
         return BotInfo(
-            name=getattr(bot_info, "name", None),
-            about=getattr(bot_info, "about", None),
-            description=getattr(bot_info, "description", None),
+            name=getattr(bot_info,"name", None),
+            about=getattr(bot_info,"about", None),
+            description=getattr(bot_info,"description", None),
+            privacy_policy_url=getattr(bot_info,"privacy_policy_url", None)
         )
