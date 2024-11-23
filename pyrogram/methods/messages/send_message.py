ea91014edf32c8f661a6da5ecdbedb972cd861a4
eyMarv <eyMarv07@gmail.com>
2024-11-23 01:08:00 +0100

    fix send_messages (v2.5.5)
    Signed-off-by: eyMarv <eyMarv07@gmail.com>

--- pyrogram/methods/messages/send_message.py
+++ pyrogram/methods/messages/send_message.py
@@ -46,6 +46,7 @@ class SendMessage:
         protect_content: bool = None,
         allow_paid_broadcast: bool = None,
         invert_media: bool = None,
+        message_effect_id: int = None,
         reply_markup: Union[
             "types.InlineKeyboardMarkup",
             "types.ReplyKeyboardMarkup",
@@ -120,6 +121,9 @@ class SendMessage:
             invert_media (``bool``, *optional*):
                 Move web page preview to above the message.
 
+            message_effect_id (``int`` ``64-bit``, *optional*):
+                Unique identifier of the message effect to be added to the message; for private chats only.
+
             reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardRemove` | :obj:`~pyrogram.types.ForceReply`, *optional*):
                 Additional interface options. An object for an inline keyboard, custom reply keyboard,
                 instructions to remove reply keyboard or to force a reply from the user.
@@ -191,6 +195,7 @@ class SendMessage:
             noforwards=protect_content,
             allow_paid_floodskip=allow_paid_broadcast,
             invert_media=invert_media,
+            effect=message_effect_id,
         )
         if business_connection_id is not None:
             r = await self.invoke(
