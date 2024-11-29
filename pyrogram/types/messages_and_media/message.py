b70406c329d403cd9968100bf36e79b41c4ed137
wulan17 <wulan17@nusantararom.org>
2024-11-29 14:20:08 +0700

    pyrofork: Add reply_to_chat_id parameter to copy_message method
    Signed-off-by: wulan17 <wulan17@nusantararom.org>
    (cherry picked from commit bba87d6e52c5e4c2c4b6462087fd3e88dbea5a3a)

--- pyrogram/types/messages_and_media/message.py
+++ pyrogram/types/messages_and_media/message.py
@@ -4793,6 +4793,7 @@ class Message(Object, Update):
         quote_text: str = None,
         quote_entities: List["types.MessageEntity"] = None,
         reply_to_message_id: int = None,
+        reply_to_chat_id: int = None,
         schedule_date: datetime = None,
         protect_content: bool = None,
         allow_paid_broadcast: bool = None,
@@ -4862,6 +4863,11 @@ class Message(Object, Update):
             reply_to_message_id (``int``, *optional*):
                 If the message is a reply, ID of the original message.
 
+            reply_to_chat_id (``int``, *optional*):
+                Unique identifier for the origin chat.
+                for reply to message from another chat.
+                You can also use chat public link in form of *t.me/<username>* (str).
+
             schedule_date (:py:obj:`~datetime.datetime`, *optional*):
                 Date when the message will be automatically sent.
 
@@ -4910,6 +4916,7 @@ class Message(Object, Update):
                 disable_notification=disable_notification,
                 message_thread_id=message_thread_id,
                 reply_to_message_id=reply_to_message_id,
+                reply_to_chat_id=reply_to_chat_id,
                 quote_text=quote_text,
                 quote_entities=quote_entities,
                 schedule_date=schedule_date,
@@ -4926,6 +4933,7 @@ class Message(Object, Update):
                 disable_notification=disable_notification,
                 message_thread_id=message_thread_id,
                 reply_to_message_id=reply_to_message_id,
+                reply_to_chat_id=reply_to_chat_id,
                 schedule_date=schedule_date,
                 has_spoiler=has_spoiler,
                 protect_content=protect_content,
@@ -5021,6 +5029,7 @@ class Message(Object, Update):
                     disable_notification=disable_notification,
                     message_thread_id=message_thread_id,
                     reply_to_message_id=reply_to_message_id,
+                    reply_to_chat_id=reply_to_chat_id,
                     quote_text=quote_text,
                     quote_entities=quote_entities,
                     schedule_date=schedule_date,
